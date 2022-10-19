ac831ce1d5ea8c9a12e0a0503895923dbc1e1869
Daria <dariastoicad@gmail.com>
2022-10-19 22:57:45 +0200

    more exercise

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import { getSingleElementValue } from "@testing-library/jest-dom/dist/utils";
+import React, { useState, useEffect } from "react";
+
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("call useEffect");
+    if (value > 0) {
+      document.title = `New Messages(${value})`;
+    }
+  }, [value]);
+  console.log("render component");
+
+  useEffect(() => {
+    console.log("hello world");
+  }, []);
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        click me!
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
